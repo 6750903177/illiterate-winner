4fce48d853c2ac55cc24249bd251b66320945d23
Ian Dahlstrom <ian.dahlstrom@gmail.com>
2023-06-26 22:42:46 -0400

    Completed BFS on binary trees with valgrind

--- c/binary_tree.c
+++ c/binary_tree.c
@@ -1,12 +1,51 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+/*
+ *  Structs and Function Prototypes
+ */
 
 typedef struct array_list {
   int length;
   int capacity;
   int *backing;
 } list_t;
+int list_init(list_t*, int);
+void list_deinit(list_t*);
+int list_push(list_t*, int);
+
+typedef struct tree {
+  int value;
+  struct tree *left;
+  struct tree *right;
+} tree_t;
+void tree_deinit(tree_t*);
+bool tree_breadth_first_search(tree_t*, int);
+void tree_pre_order(tree_t*, list_t*);
+void tree_in_order(tree_t*, list_t*);
+void tree_post_order(tree_t*, list_t*);
+
+typedef struct node {
+  tree_t *value;
+  struct node *next;
+} node_t;
+
+typedef struct queue {
+  node_t *head;
+  node_t *tail;
+  int length;
+} queue_t;
+void queue_init(queue_t*);
+void queue_deinit(queue_t*);
+void queue_push(queue_t*, tree_t*);
+int queue_pop(queue_t*, tree_t**);
+
+
+/*
+ *  Function Implementations
+ */
 
 int list_init(list_t *l, int capacity) {
   l->length = 0;
@@ -33,13 +72,7 @@ int list_push(list_t *l, int value) {
   return 0;
 }
 
-typedef struct node {
-  int value;
-  struct node *left;
-  struct node *right;
-} node_t;
-
-void tree_deinit(node_t *n) {
+void tree_deinit(tree_t *n) {
   if (n == NULL) {
     return;
   }
@@ -49,7 +82,33 @@ void tree_deinit(node_t *n) {
   free(n);
 }
 
-void tree_pre_order(node_t *n, list_t *path) {
+bool tree_breadth_first_search(tree_t *n, int needle) {
+  bool found = false;
+  queue_t *buf = malloc(sizeof(queue_t));
+  queue_init(buf);
+  queue_push(buf, n);
+
+  tree_t *curr;
+  while ((queue_pop(buf, &curr)) == 0) {
+    if (curr->value == needle) {
+      found = true;
+      break;
+    }
+
+    if (curr->left != NULL) {
+      queue_push(buf, curr->left);
+    }
+
+    if (curr->right != NULL) {
+      queue_push(buf, curr->right);
+    }
+  }
+
+  queue_deinit(buf);
+  return found;
+}
+
+void tree_pre_order(tree_t *n, list_t *path) {
   if (n == NULL) {
     return;
   }
@@ -59,7 +118,7 @@ void tree_pre_order(node_t *n, list_t *path) {
   tree_pre_order(n->right, path);
 }
 
-void tree_in_order(node_t *n, list_t *path) {
+void tree_in_order(tree_t *n, list_t *path) {
   if (n == NULL) {
     return;
   }
@@ -69,7 +128,7 @@ void tree_in_order(node_t *n, list_t *path) {
   tree_in_order(n->right, path);
 }
 
-void tree_post_order(node_t *n, list_t *path) {
+void tree_post_order(tree_t *n, list_t *path) {
   if (n == NULL) {
     return;
   }
@@ -78,6 +137,61 @@ void tree_post_order(node_t *n, list_t *path) {
   tree_post_order(n->right, path);
   list_push(path, n->value);
 }
+
+void queue_init(queue_t *q) {
+  q->head = NULL;
+  q->tail = NULL;
+  q->length = 0;
+  return;
+}
+
+void queue_deinit(queue_t *q) {
+  while (q->head != NULL) {
+    q->length--;
+    node_t *curr = q->head;
+    q->head = curr->next;
+    free(curr);
+  }
+  free(q);
+}
+
+void queue_push(queue_t *q, tree_t *value) {
+  q->length++;
+  node_t *n = malloc(sizeof(node_t));
+  n->value = value;
+  n->next = NULL;
+
+  if (q->tail == NULL) {
+    q->head = n;
+    q->tail = n;
+  } else {
+    q->tail->next = n;
+    q->tail = n;
+  }
+}
+
+int queue_pop(queue_t *q, tree_t **value) {
+  if (q->head == NULL) {
+    return 1;
+  }
+
+  q->length--;
+  node_t *curr = q->head;
+  *value = curr->value;
+  if (q->length == 0) {
+    q->head = NULL;
+    q->tail = NULL;
+  } else {
+    q->head = curr->next;
+  }
+  free(curr);
+  return 0;
+}
+
+
+/*
+ *  Main
+ */
 
 int main() {
   /*
@@ -97,43 +211,43 @@ int main() {
    *          +- 7
    *       
    */
-  node_t *n7 = malloc(sizeof(node_t));
+  tree_t *n7 = malloc(sizeof(tree_t));
   n7->value = 7;
   n7->right = NULL;
   n7->left = NULL;
-  node_t *n5 = malloc(sizeof(node_t));
+  tree_t *n5 = malloc(sizeof(tree_t));
   n5->value = 5;
   n5->right = n7;
   n5->left = NULL;
-  node_t *n15 = malloc(sizeof(node_t));
+  tree_t *n15 = malloc(sizeof(tree_t));
   n15->value = 15;
   n15->right = NULL;
   n15->left = NULL;
-  node_t *n10 = malloc(sizeof(node_t));
+  tree_t *n10 = malloc(sizeof(tree_t));
   n10->value = 10;
   n10->right = n15;
   n10->left = n5;
-  node_t *n29 = malloc(sizeof(node_t));
+  tree_t *n29 = malloc(sizeof(tree_t));
   n29->value = 29;
   n29->right = NULL;
   n29->left = NULL;
-  node_t *n45 = malloc(sizeof(node_t));
+  tree_t *n45 = malloc(sizeof(tree_t));
   n45->value = 45;
   n45->right = NULL;
   n45->left = NULL;
-  node_t *n30 = malloc(sizeof(node_t));
+  tree_t *n30 = malloc(sizeof(tree_t));
   n30->value = 30;
   n30->right = n45;
   n30->left = n29;
-  node_t *n100 = malloc(sizeof(node_t));
+  tree_t *n100 = malloc(sizeof(tree_t));
   n100->value = 100;
   n100->right = NULL;
   n100->left = NULL;
-  node_t *n50 = malloc(sizeof(node_t));
+  tree_t *n50 = malloc(sizeof(tree_t));
   n50->value = 50;
   n50->right = n100;
   n50->left = n30;
-  node_t *n20 = malloc(sizeof(node_t));
+  tree_t *n20 = malloc(sizeof(tree_t));
   n20->value = 20;
   n20->right = n50;
   n20->left = n10;
@@ -165,7 +279,10 @@ int main() {
   }
   list_deinit(sol);
 
-  tree_deinit(n20);
+  assert( tree_breadth_first_search(n20, 45));
+  assert( tree_breadth_first_search(n20, 7));
+  assert(!tree_breadth_first_search(n20, 69));
 
+  tree_deinit(n20);
   return 0;
 }
